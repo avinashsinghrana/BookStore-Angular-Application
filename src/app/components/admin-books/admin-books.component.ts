9917e848f7799524856970ff66b419a4d4bebd67
Naveen-07 <naveennani0711@gmail.com>
2020-07-14 14:52:47 +0530

    [Naveen] Refactor. admin dashboard Component

--- src/app/components/admin-books/admin-books.component.ts
+++ src/app/components/admin-books/admin-books.component.ts
@@ -1,10 +1,9 @@
 import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
-import { SellerService } from "../../services/seller.service";
-import { MessageService } from "../../services/message.service";
+import { SellerService } from '../../services/seller.service';
+import { MessageService } from '../../services/message.service';
 import { MatSnackBar, MatDialog } from '@angular/material';
 import { Book } from 'src/app/models/book.model';
-
 
 @Component({
   selector: 'app-admin-books',
@@ -25,24 +24,26 @@ export class AdminBooksComponent implements OnInit {
     private snackBar: MatSnackBar,
     private data: MessageService,
     private dialog: MatDialog,
-    private userService : UserService
+    private userService: UserService
   ) {}
 
   ngOnInit() {
-   
-   // this.sortTerm = 'none';
+    // this.sortTerm = 'none';
     this.messageService.currentBooks.subscribe((data) => {
       this.books = [];
       this.onDisplayBooks(data);
     });
-    this.messageService.currentEvent$.subscribe(message =>
-      { this.searchTerm = message});
+    this.messageService.currentEvent$.subscribe((message) => {
+      this.searchTerm = message;
+    });
+    if (localStorage.getItem('token') != null) {
       this.getAllBooks();
+    }
   }
   onBookDetail(event) {
     event.stopPropagation();
   }
-/*
+  /*
   onSelect(val: any){
    this.sortTerm = val;
    console.log("sorting term",this.sortTerm);
@@ -53,53 +54,53 @@ export class AdminBooksComponent implements OnInit {
         if (data.status === 200) {
           this.messageService.changeBooks();
           this.ngOnInit();
-          this.snackBar.open("Book approved successfully", 'ok', {
+          this.snackBar.open('Book approved successfully', 'ok', {
             duration: 2000,
           });
         }
       },
       (error) => {
-        this.snackBar.open("Failed to Approved", 'ok', {
+        this.snackBar.open('Failed to Approved', 'ok', {
           duration: 2000,
         });
       }
     );
   }
-  
+
   onDisplayBooks(data) {
     console.log(data);
     if (data.status === 200) {
       this.size = data.data.length;
       data.data.forEach((bookData) => {
-      this.books.push(bookData);
-        
+        this.books.push(bookData);
       });
     }
   }
 
-bookList : [];
-getAllBooks() {
-  this.userService.getAllBooks().subscribe((Response: any) => {
-  //    this.unVerifiedBooks = Response.obj;
- 
-  console.log(Response);
-  this.bookList = Response.data;
-  this.size = Response.data.length;
-  })
-}
+  bookList: [];
+  getAllBooks() {
+    this.userService.getAllBooks().subscribe((Response: any) => {
+      //    this.unVerifiedBooks = Response.obj;
+
+      console.log(Response);
+      this.bookList = Response.data;
+      this.size = Response.data.length;
+    });
+  }
   onDisapproval(bookId) {
     console.log(bookId);
     this.vendorService.deleteBooks(bookId).subscribe(
-      (data) => {      
+      (data) => {
         this.messageService.changeBooks();
-          this.snackBar.open("Book disapproved successfully", 'ok', {
-            duration: 2000,
-          })
+        this.snackBar.open('Book disapproved successfully', 'ok', {
+          duration: 2000,
+        });
       },
       (error: any) => {
-        this.snackBar.open("Failed to disapproved book", 'ok', { duration: 2000 });
+        this.snackBar.open('Failed to disapproved book', 'ok', {
+          duration: 2000,
+        });
       }
     );
   }
- 
 }
